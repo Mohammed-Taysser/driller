fe1b9ccf4404063ff60aeb319af00c80c0a1fcc8
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-03-07 21:18:55 +0200

    create lazLoading component

---
+++ src/apps/lazyLoading.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Homepage = React.lazy(() => import('../pages/Homepage'));
+const AboutUs = React.lazy(() => import('../pages/AboutUs'));
+const ContactUs = React.lazy(() => import('../pages/ContactUs'));
+const Projects = React.lazy(() => import('../pages/Projects'));
+const ProjectDetails = React.lazy(() => import('../pages/ProjectDetails'));
+const Services = React.lazy(() => import('../pages/Services'));
+const ServiceDetails = React.lazy(() => import('../pages/ServiceDetails'));
+const PageNotFound = React.lazy(() => import('../pages/404'));
+
+export const PUBLIC_ROUTES = [
+	{
+		path: '/',
+		component: Homepage,
+	},
+	{
+		path: '/about-us',
+		component: AboutUs,
+	},
+	{
+		path: '/contact-us',
+		component: ContactUs,
+	},
+	{
+		path: '/projects',
+		component: Projects,
+	},
+	{
+		path: '/single-project',
+		component: ProjectDetails,
+	},
+	{
+		path: '/services',
+		component: Services,
+	},
+	{
+		path: '/single-service',
+		component: ServiceDetails,
+	},
+	{
+		path: '*',
+		component: PageNotFound,
+	},
+];
