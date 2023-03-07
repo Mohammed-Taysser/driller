824458f1229bc24158bc2aa2fba191567b930ab7
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-03-07 21:19:04 +0200

    create routes component

---
+++ src/apps/routes.tsx
@@ -0,0 +1,44 @@
+import React, { ReactElement, useEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { PUBLIC_ROUTES } from './lazyLoading';
+import Layout from '../layouts';
+import Spinner from '../components/Spinner';
+
+interface SuspenseWrapperProps {
+	children: ReactElement;
+}
+
+const SuspenseWrapper = (props: SuspenseWrapperProps) => {
+	return (
+		<React.Suspense fallback={<Spinner />}>{props.children}</React.Suspense>
+	);
+};
+
+function MainRoutes() {
+	const location = useLocation();
+
+	useEffect(() => {
+		window.scrollTo({ top: 0 });
+		// scroll to the top of the browser window when changing route
+	}, [location]);
+
+	return (
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				{PUBLIC_ROUTES.map((route) => (
+					<Route
+						path={route.path}
+						key={route.path}
+						element={
+							<SuspenseWrapper>
+								<route.component />
+							</SuspenseWrapper>
+						}
+					/>
+				))}
+			</Route>
+		</Routes>
+	);
+}
+
+export default MainRoutes;
