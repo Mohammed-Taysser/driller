fc970dbd4aae3d231d230a766ecff01b2a3cd3ea
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-03-17 14:45:42 +0200

    add navbar interfaces

--- src/apps/react-app-env.d.ts
+++ src/apps/react-app-env.d.ts
@@ -8,3 +8,37 @@ interface ErrorBoundaryState {
 	error: Error | null;
 	errorInfo: ErrorInfo | null;
 }
+
+interface StickyNavbarProps {
+	navbarHumbugger: React.RefObject<SVGSVGElement>;
+	toggleMobileNavigation: () => void;
+	onSearchBtnClick: (
+		evt: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+	) => void;
+}
+
+interface SearchPopupProps {
+	formInputRef: React.RefObject<HTMLInputElement>;
+	searchBoxRef: React.RefObject<HTMLDivElement>;
+	searchFormRef: React.RefObject<HTMLFormElement>;
+	submitBtnRef: React.RefObject<HTMLButtonElement>;
+}
+
+interface MobileNavbarProps {
+	menuRef: React.RefObject<HTMLDivElement>;
+	navbarHumbuggerRef: React.RefObject<SVGSVGElement>;
+	toggleMobileNavigation: () => void;
+	onSearchBtnClick: (
+		evt: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+	) => void;
+}
+
+interface SingleLinkWithMenu {
+	path: IntrinsicAttributes & LinkProps & RefAttributes<HTMLAnchorElement>;
+	title: String;
+}
+interface LinkWithMenuProps {
+	title: String;
+	path: IntrinsicAttributes & LinkProps & RefAttributes<HTMLAnchorElement>;
+	items: SingleLinkWithMenu[];
+}
